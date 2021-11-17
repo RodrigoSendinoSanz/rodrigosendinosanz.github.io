ddd3d7a12bd539283c5e703a8007f926a7d16615
RodrigoSendinoSanz <78493406+RodrigoSendinoSanz@users.noreply.github.com>
2021-11-18 00:15:55 +0100

    1comit

---
+++ js/svgdraw.js
@@ -0,0 +1,45 @@
+var imgBx = document.querySelector('.imgBx svg')
+var btnDraw = document.querySelector('.btn-draw')
+var btnErase = document.querySelector('.btn-erase')
+
+// We are only adding and removing the 'active' class,
+// the entire animation is defined in the CSS code
+window.onload = function() {
+  draw();
+};
+
+
+
+
+function draw() {
+  imgBx.classList.add('active')
+  console.log("%c Bienvenid@ a mi pagina", "background: #ff8906; border-radius:12%;color:#fffffe;padding: 2em;font-size: 2em;");
+  console.log("%c No olvides seguirme en redes:", "color:#e53170;padding: 1.5em;font-size: 1.5em;"); 
+  console.log("%c Instagram: https://www.instagram.com/rodrigosendinosanz/", "color:#405DE6;padding: 1em;font-size: 1em;background: #fff;");
+  console.log("%c Twitter: https://twitter.com/rodrigosendino", "color:#1DA1F2;padding: 1em;font-size: 1em;background: #fff;");
+  console.log("%c Linkedin: https://www.linkedin.com/in/rodrigo-sendino-sanz-27a3a0100/", "color:#0e76a8;padding: 1em;font-size: 1em;background: #fff;");
+  console.log("%c Github: https://github.com/RodrigoSendinoSanz", "color:#211F1F;padding: 1em;font-size: 1em;background: #fff;");
+  console.log("%c by Rodrigo Sendino Sanz", "color:gold;padding: 1em;font-size: 1.5em;");
+  console.log("%c                     @@@@@@@                     ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c             @...@@           @@...@             ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c        @..@                       @..@         ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c     @..        @..@    /.@   @@..@      ..@      ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c   @.@     @.@         /.@         *.#    @.@    ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c  .@    ,.@            /.@            .@    (.   ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c  %..@ .@              /.@              .@   @.@ ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c       @...@           /.@               @@   @.@","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c           @....%      /.@                .@   @.","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c               &....@  /.&                 .    .","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c@.... (.............@ @...*    ..............  ...","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c@.    @&                /./....%                  ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c .@    .                /./   &....@              ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c @.     .               /./        @...@          ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c  @.&    .@             /./            @..@       ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c    .@     .@           /./            .@  @...   ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c     @.@     @.@        /./         ..     @.@    ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c       @./       @..@@  /./   @...@      ..@      ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c          @..@                       @..@         ","color:gold;padding: -1em;font-size: 1.5em;");
+  console.log("%c              @...@#           @@...@             ","color:gold;padding: -1em;font-size: 1.5em;");
+
+
+}
